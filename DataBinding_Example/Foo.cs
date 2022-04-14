0a19c1fdc3e5a65265f9bcf71c02bb33ec1c31f1
zelus <fishxd1096@gmail.com>
2022-04-14 20:01:50 +0800

    加入專案檔案。

---
+++ DataBinding_Example/Foo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace DataBinding_Example;
+
+public class Foo : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    public int Number
+    {
+        get
+        {
+            return _number;
+        }
+        set 
+        {
+            if (value == _number)
+                return;
+            _number = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Number)));
+        }
+    }
+    private int _number;
+}
